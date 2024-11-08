d2136f834c458f0c80d84abe97f1d3f0194f4b9c
raphckrman <41128238+raphckrman@users.noreply.github.com>
2024-11-08 18:29:06 +0100

    fix(canteen): set Identification in authentication

--- src/services/izly/reload.ts
+++ src/services/izly/reload.ts
@@ -2,7 +2,7 @@ import type { IzlyAccount } from "@/stores/account/types";
 import {Identification, refresh} from "ezly";
 
 export const reload = async (account: IzlyAccount): Promise<Identification> => {
-    const instance = account.instance!
+    const instance = account.authentication.identification
     const secret = account.authentication.secret
 
     await refresh(instance, secret);
